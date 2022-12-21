762f6878c9b9b832b81a4fd2500f9368e5d45b8d
Zahed Alam <zahed@innof.com>
2022-12-21 01:49:51 +0100

    Sidebar updated

--- crypto-wallet/tailwind.config.js
+++ crypto-wallet/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        txtMain: '#1A3163',
+      },
+    },
   },
   plugins: [],
 }
